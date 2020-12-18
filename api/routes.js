aac2d5045d3f1082d9bd00ab9249f0366a037ba6
Aleksandar Dimitrov <aleksandar.dimitrov@and.digital>
2020-12-18 15:55:37 +0200

    use appropriate method

--- api/routes.js
+++ api/routes.js
@@ -4,8 +4,8 @@ function routes(server) {
   server.get("/locations", controller.getLocations);
   server.get("/location/:id/products", controller.getProductsByLocation);
   server.get("/stock", controller.getProductStockAtLocation);
-  server.get("/:id/add-product", controller.addProductToBasket);
-  server.get("/checkout", controller.checkout);
+  server.post("/:id/add-product", controller.addProductToBasket);
+  server.post("/checkout", controller.checkout);
 
 }
 
